4d066fa90d4173850ed58b6f16f33de72695eec8
Kris Cox <kris.cox@cococo.be>
2018-02-02 22:53:17 +0100

    Second upload

---
+++ Kostuumotheek/Models/Costume.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Kostuumotheek.Models
+{
+    public class Costume
+    {
+        public Costume()
+        {
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+        }
+    }
+}
