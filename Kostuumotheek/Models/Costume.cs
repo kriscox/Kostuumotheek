30663b7076e001ffb414c523f308aa966feef005
Kris Cox <kris.cox@cococo.be>
2018-02-08 21:43:14 +0100

    bb

--- Kostuumotheek/Models/Costume.cs
+++ Kostuumotheek/Models/Costume.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Kostuumotheek.Models
 {
     public class Costume
     {
         public int ID { get; set; }
+
+        [Display(Name = "Naam")]
+        [StringLength(20, MinimumLength = 3)]
+        [Required]
         public string Name { get; set; }
+
+        [Display(Name = "Omschrijving")]
+        [StringLength(60, MinimumLength = 0)]
         public string Description { get; set; }
     }
 }
--- Kostuumotheek/Startup.cs
+++ Kostuumotheek/Startup.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Kostuumotheek.Data;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc.Razor;
 
 namespace Kostuumotheek
 {
@@ -19,9 +22,50 @@ namespace Kostuumotheek
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                // Add support for finding localized views, based on file name suffix, e.g. Index.fr.cshtml
+                .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+                    // Add support for localizing strings in data annotations (e.g. validation messages) via the
+                    // IStringLocalizer abstractions.
+                    .AddDataAnnotationsLocalization();
 
             services.AddDbContext<KostuumotheekContext>( options => options.UseMySQL("server=192.168.252.2;port=3307;database=Kostuumotheek;user=www;password=w8chtw00rd"));
+
+            // Configure supported cultures and localization options
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new[]
+                {
+                    new CultureInfo("nl")
+                    //new CultureInfo("fr"),
+                    //new CultureInfo("en-US")
+
+                };
+
+                // State what the default culture for your application is. This will be used if no specific culture
+                // can be determined for a given request.
+                options.DefaultRequestCulture = new RequestCulture(culture: "nl", uiCulture: "nl");
+
+                // You must explicitly state which cultures your application supports.
+                // These are the cultures the app supports for formatting numbers, dates, etc.
+                options.SupportedCultures = supportedCultures;
+
+                // These are the cultures the app supports for UI strings, i.e. we have localized resources for.
+                options.SupportedUICultures = supportedCultures;
+
+                // You can change which providers are configured to determine the culture for requests, or even add a custom
+                // provider with your own logic. The providers will be asked in order to provide a culture for each request,
+                // and the first to provide a non-null result that is in the configured supported cultures list will be used.
+                // By default, the following built-in providers are configured:
+                // - QueryStringRequestCultureProvider, sets culture via "culture" and "ui-culture" query string values, useful for testing
+                // - CookieRequestCultureProvider, sets culture via "ASPNET_CULTURE" cookie
+                // - AcceptLanguageHeaderRequestCultureProvider, sets culture via the "Accept-Language" request header
+                //options.RequestCultureProviders.Insert(0, new CustomRequestCultureProvider(async context =>
+                //{
+                //  // My custom request culture logic
+                //  return new ProviderCultureResult("en");
+                //}));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
